e2d2ddf8051426f37902d31b12e9e98da0735d6a
Mary <maryrosea@missionreadyhq.com>
2024-09-10 12:31:43 +1200

    modify jest.config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,10 @@ const config = {
   transformIgnorePatterns: [
     "/node_modules/(?!(module1|module2)/)"
   ],
-  testEnvironment: "node"
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.jsx?$": ["babel-jest", { presets: ["@babel/preset-env"], plugins: ["@babel/plugin-transform-modules-commonjs"] }]
+  }
 };
 
 export default config;
