ee6fc3864ec79676b80ce062e83a9c594135cea1
Mary <maryrosea@missionreadyhq.com>
2024-09-10 12:08:36 +1200

    change config.js to transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const config = {
   transform: {
     "^.+\\.jsx?$": "babel-jest"
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(module1|module2)/)"
+  ],
   testEnvironment: "node"
 };
 
